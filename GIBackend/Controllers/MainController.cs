3ab2e3c3440c0cc01d2759100c76ca5f5dc74b2e
ramzes1992 <adrian.ramzes@gmail.com>
2016-01-24 17:59:49 +0100

    Added GetGDPRanking

--- GIBackend/Controllers/MainController.cs
+++ GIBackend/Controllers/MainController.cs
@@ -3,20 +3,48 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 using System.Web;
+using System.Web.Hosting;
 using System.Web.Mvc;
 
 namespace GIBackend.Controllers
 {
     public class MainController : Controller
     {
+        public static Dictionary<int, string> CountryCodes = new Dictionary<int, string>();
+
+        public MainController()
+        {
+            //var path = HttpContext.Server.MapPath("~/Data/country-codes.csv");
+            if (!CountryCodes.Any())
+            {
+                var path = HostingEnvironment.MapPath("~/Data/country-codes.csv");
+                var countryCodesLines = System.IO.File.ReadAllLines(path);
+                foreach (var line in countryCodesLines)
+                {
+                    if (line.Contains("\""))
+                        continue;
+                    var values = line.Split(',');
+                    CountryCodes.Add(int.Parse(values[4]), values[0]);
+                }
+            }
+        }
+
         // GET: Main
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult Ranking()
+        public ActionResult Ranking(int? id)
         {
+            if (id.HasValue)
+            {
+                if (CountryCodes.ContainsKey(id.Value))
+                {
+                    var countryName = CountryCodes[id.Value];
+                }
+            }
+
             return View();
         }
 
@@ -47,6 +75,35 @@ namespace GIBackend.Controllers
             }
 
             return Json(items, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult GetGDPRanking(int? countryCode)
+        {
+            var path = HttpContext.Server.MapPath("~/Data/pkb.csv");
+            var data = System.IO.File.ReadAllLines(path);
+
+            List<GDPRanking> items = new List<GDPRanking>();
+
+            for (int i = 1; i < data.Length; i++)//first row has only columns names
+            {
+                var columnValues = data[i].Split(',');
+
+                items.Add(new GDPRanking()
+                {
+                    country = columnValues[0],
+                    year = int.Parse(columnValues[1]),
+                    value = int.Parse(columnValues[2]),
+                });
+            }
+
+            return Json(items, JsonRequestBehavior.AllowGet);
+        }
+
+        class GDPRanking
+        {
+            public string country { get; set; }
+            public int year { get; set; }
+            public int value { get; set; }
         }
 
         class GDPDataItem
